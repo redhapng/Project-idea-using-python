abef6ef573e749ac4c650565a6e9c4eed2c2e163
Reda Ait babaaziz <89072817+redhapng@users.noreply.github.com>
2024-10-22 17:52:48 +0100

    Add files via upload
    months_in_letters

---
+++ months_in_letters.py
@@ -0,0 +1,13 @@
+months = {1:"January",2:"February",
+3:"March",4:"April",
+5:"May",6:"June",
+7:"July",8:"August",
+9:"September",10:"October",
+11:"November",12:"December"}
+#----------------------------------------------------------------------------------------
+
+
+
+
+mnths_in_nums = int(input("Enter the months number : \n"))
+pri = print(months.get(mnths_in_nums))
